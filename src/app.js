1486ffe8f8cea528ffd58d18c40044300512d041
kelseymayhem <kelsey.mayhem@gmail.com>
2021-03-30 01:18:20 -0400

    Added fahrenheit conversion
    Also learned how to spell fahrenheit forever

--- src/app.js
+++ src/app.js
@@ -30,6 +30,8 @@ function displayTemperature(response) {
  let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
 
+celsiusTemp = response.data.main.temp;
+
  temperatureElement.innerHTML = Math.round(response.data.main.temp);
  cityElement.innerHTML = response.data.name; 
  descriptionElement.innerHTML = response.data.weather[0].description;
@@ -52,8 +54,19 @@ let cityInputElement = document.querySelector("#city-input")
 search(cityInputElement.value);
 }
 
-search("New York");
+function showFahrenheitTemp(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
 
+let celsiusTemp = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+
+search("New York");
