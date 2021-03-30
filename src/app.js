9614f6712b6f3105882243385dd34aae1949dfe5
kelseymayhem <kelsey.mayhem@gmail.com>
2021-03-29 22:42:58 -0400

    Update app.js
    + `${city} to API call

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "ce2a122a9141d0af01c35f1399c43afe";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature)
