9e607e1ff0885e81b77da39a9cff4602c5bd7fce
kelseymayhem <kelsey.mayhem@gmail.com>
2021-03-29 21:21:12 -0400

    Current Temp Updates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 function displayTemperature(response) {
- console.log(response.data.main.temp);   
+ console.log(response.data.main.temp);
+ let temperatureElement = document.querySelector("#temperature");
+ let cityElement = document.querySelector("#city");
+ let descriptionElement = document.querySelector("#description");
+ let humidityElement = document.querySelector("#humidity")
+ let speedElement = document.querySelector("#speed")
+ temperatureElement.innerHTML = Math.round(response.data.main.temp);
+ cityElement.innerHTML = response.data.name; 
+ descriptionElement.innerHTML = response.data.weather[0].description;
+ humidityElement.innerHTML = response.data.main.humidity;
+ speedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "ce2a122a9141d0af01c35f1399c43afe";
