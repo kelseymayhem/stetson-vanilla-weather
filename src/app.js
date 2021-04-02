80b3456c4d21670b154de8ca276d010571b27063
kelseymayhem <kelsey.mayhem@gmail.com>
2021-04-02 12:37:48 -0400

    added API integration for forecast

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`; 
     let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -36,6 +36,12 @@ function displayForecast() {
    
             forecastHTML = forecastHTML + `</div`;
     forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey = "ce2a122a9141d0af01c35f1399c43afe";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -56,6 +62,8 @@ celsiusTemp = response.data.main.temp;
  dateElement.innerHTML = formatDate(response.data.dt * 1000);
  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -99,4 +107,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("New York");
-displayForecast();
